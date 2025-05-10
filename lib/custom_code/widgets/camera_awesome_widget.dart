fd2dc669e92ccb3d6f3c9a10a65dbf9a5f4a26b4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-10 16:11:37 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/widgets/camera_awesome_widget.dart
@@ -0,0 +1,28 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class CameraAwesomeWidget extends StatefulWidget {
+  const CameraAwesomeWidget({
+    super.key,
+    this.width,
+    this.height,
+  });
+
+  final double? width;
+  final double? height;
+
+  @override
+  State<CameraAwesomeWidget> createState() => _CameraAwesomeWidgetState();
+}
+
+class _CameraAwesomeWidgetState extends State<CameraAwesomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
